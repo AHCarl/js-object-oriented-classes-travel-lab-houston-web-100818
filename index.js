6cbc758f8f19ae656f785f539c9681a0f073650a
Alexander Carlin <ahcarlin@gmail.com>
2018-11-30 20:08:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation){
+    
+  }
+}
